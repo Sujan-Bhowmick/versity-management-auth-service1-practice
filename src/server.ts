46bde7c68615bc62e5cc9a7c97363849f7798f71
Your Name <imsujan93@gmail.com>
2023-06-29 17:55:58 +0600

    Improve lint setup

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import app from './app'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
+    // eslint-disable-next-line no-console
     console.log(`Database is connected successfully`)
 
     app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
       console.log(`Example app listening on port ${config.port}`)
     })
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(`Failed to connect database`, err)
   }
 }
