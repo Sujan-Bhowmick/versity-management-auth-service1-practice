9d0265a2a2b541a89ad59af1207dd351e16008bf
Your Name <imsujan93@gmail.com>
2023-07-18 17:17:13 +0600

    fff

--- src/app/modules/academicFaculty/academicFaculty.interface.ts
+++ src/app/modules/academicFaculty/academicFaculty.interface.ts
@@ -4,10 +4,14 @@ export type IAcademicFacultyTitle =
   | 'Science and Engineering'
   | 'Business Administration'
   | 'Arts and Social Science';
+
 export type IAcademicFaculty = {
   title: IAcademicFacultyTitle;
 };
 
-export type AcademicFacultyModel = Model<IAcademicFaculty>;
+export type AcademicFacultyModel = Model<
+  IAcademicFaculty,
+  Record<string, unknown>
+>;
 
 export type IAcademicFacultyFilters = { searchTerm?: string };
